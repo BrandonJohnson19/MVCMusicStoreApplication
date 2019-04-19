207c66ed5eeae5ad6a30313d50cca1a9009f9c9e
Brandon <s01100303@acad.tri-c.edu>
2019-04-18 20:45:25 -0400

    Week 10 commit

---
+++ MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        public ActionResult Browse()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        public ActionResult Index(int id)
+        {
+            var albums = GetAlbums(id);
+            return View("Index", albums);
+        }
+
+        private List<Album> GetAlbums(int searchInt)
+        {
+            return db.Albums
+            .Where(a => a.GenreId.Equals(searchInt))
+            .ToList();
+        }
+
+        public ActionResult Details(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View("Details", album);
+        }
+    }
+}
